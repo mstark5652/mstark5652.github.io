d1dfd4e9c4bd5327657ab23797d74529abc2b44e
Michael Stark <mstark5652@gmail.com>
2019-09-02 12:48:53 -0500

    style clean for header/nav width

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -21,12 +21,14 @@ export class Navigation extends React.Component<object, NavState> {
 
   render() {
     return (
-      <div id='navbar'>
-        <a className={(this.state.activePage == '' || this.state.activePage == 'home') ? 'active' : ''} onClick={() => this.pageState('home')} href='#'>Home</a>
-        <a className={(this.state.activePage == 'about') ? 'active' : ''} onClick={() => this.pageState('about')} href='#about'>About</a>
-        <a className={(this.state.activePage == 'projects') ? 'active' : ''} onClick={() => this.pageState('projects')} href='#projects'>Projects</a>
-        <a className={(this.state.activePage == 'contact') ? 'active' : ''} onClick={() => this.pageState('contact')} href='#contact'>Contact</a>
-      </div>
+      <nav>
+        <div className='items'>
+          <a className={(this.state.activePage == '' || this.state.activePage == 'home') ? 'active' : ''} onClick={() => this.pageState('home')} href='#'>Home</a>
+          <a className={(this.state.activePage == 'about') ? 'active' : ''} onClick={() => this.pageState('about')} href='#about'>About</a>
+          <a className={(this.state.activePage == 'projects') ? 'active' : ''} onClick={() => this.pageState('projects')} href='#projects'>Projects</a>
+          <a className={(this.state.activePage == 'contact') ? 'active' : ''} onClick={() => this.pageState('contact')} href='#contact'>Contact</a>
+        </div>
+      </nav>
     );
   }
 }
