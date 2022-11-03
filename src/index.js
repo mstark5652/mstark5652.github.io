0c458ec1cac1b24b8def5923b761a5bd0ff9dfef
mstark5652 <mstark5652@gmail.com>
2022-11-03 09:53:17 -0500

    chore: clean up event

--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import AppContainer from './App'
-import EventContainer from './EventContainer'
 
 // leave - used for including styles in build
 import './styles/main'
 
 if (typeof window !== 'undefined') {
-  if (document.title === 'Crema Event') {
-    window.onload = () => {
-      ReactDOM.render(<EventContainer />, document.getElementById('main'))
-    }
-  } else {
-    window.onload = () => {
-      ReactDOM.render(<AppContainer />, document.getElementById('main'))
-    }
+  window.onload = () => {
+    ReactDOM.render(<AppContainer />, document.getElementById('main'))
   }
 }
