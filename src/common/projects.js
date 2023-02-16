5608542e529459c9fd6d398be69ab40a8ba6e340
mstark5652 <mstark5652@gmail.com>
2023-02-16 15:35:56 -0600

    fix: links

--- src/common/projects.js
+++ src/common/projects.js
@@ -76,7 +76,7 @@ export const projects = [
     description: 'Mixologist is the world\'s first voice-powered robotic bartender that uses natural language to take your order and serve your favorite mixed drinks.',
     link: {
       text: 'Drink Up',
-      to: 'https://www.mixologist.ai?cid=mstark.dev'
+      to: 'https://www.instagram.com/mixologist.ai/?cid=mstark.dev'
     },
     img: {
       src: require('../img/mix_cosmo.png'),
@@ -88,7 +88,7 @@ export const projects = [
     description: 'RightRoom is software that enables attendees to enter a simulated room and fluidly engage in one-on-one and group video conversations.',
     link: {
       text: 'Join the Conversation',
-      to: 'https://rightroom.io'
+      to: 'https://www.linkedin.com/company/rightroom/?cid=mstark.dev'
     },
     img: {
       src: require('../img/rr_chat.png'),
@@ -101,7 +101,7 @@ export const projects = [
     description: 'HumanKind is a decentralized marketplace that aggregates, sponsors and rewards altruistic behavior and random acts of kindness, giving back to the communities, businesses and charities you care about. Essentially, the kinder you are, the more you—and the community you are a part of—get back. My team started working on this at the Hedera20 hackathon where we walked away with multiple prizes.',
     link: {
       text: 'Be HumanKind',
-      to: 'https://humankind.ly'
+      to: 'https://humankind.ly?cid=mstark.dev'
     },
     img: {
       src: require('../img/hk_logo.png'),
@@ -113,7 +113,7 @@ export const projects = [
     description: 'Price guide and tracking for all your comic books.',
     link: {
       text: 'Explore',
-      to: 'https://comicbookrealm.com'
+      to: 'https://comicbookrealm.com?cid=mstark.dev'
     },
     img: {
       src: require('../img/cbr_screens.png'),
@@ -125,7 +125,7 @@ export const projects = [
     description: 'Welcome to the Neighborhood. A hyper-local, real-time, real-life network and exchange made up of your actual neighbors that rewards "neighborly" acts of kindness and brings us all closer together.',
     link: {
       text: 'Be Neighborly',
-      to: 'https://www.intouchsol.com/blog/intouch-team-wins-big-hack-midwest-competition/'
+      to: ''
     },
     img: {
       src: require('../img/neighborhood_alexa.png'),
